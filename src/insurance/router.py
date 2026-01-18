40633b208096ad117da3ee611135565d64a43277
atharvpande77 <atharvpande@social-impact.in>
2026-01-19 00:25:45 +0530

    190120260021

--- src/insurance/router.py
+++ src/insurance/router.py
@@ -176,21 +176,22 @@ async def police_whatsapp_chat_webhook(request: Request, session: Annotated[Asyn
         
         station_info = await get_curr_location_jurisdiction_and_nearest_station(session, lat, lon)
         
-        juridiction_station = station_info.get("containing_station", {})
-        nearest_station = station_info.get("nearest_station", {})
-        
-        final_message = f"""
-            You are currently in jurisdiction of:\n
-            {juridiction_station.get("name", "unknown")}\n
-            Address: {juridiction_station.get("address", "N/A")}\n\n
-            Google Maps Link: https://www.google.com/maps/dir/?api=1&destination={quote_plus(juridiction_station.get("address", 'N/A'))}&travelmode=driving&dir_action=navigate
-            \n\n           
+        try:
+            juridiction_station = station_info.get("containing_station", {})
+            nearest_station = station_info.get("nearest_station", {})
             
-            Nearest police station: {nearest_station.get("name", "unknown")} ({(nearest_station.get("distance_meters", 0)/1000).__format__(".2f")} km away)\n
-            Google Maps Link: https://www.google.com/maps/dir/?api=1&destination={quote_plus(nearest_station.get("address", 'N/A'))}&travelmode=driving&dir_action=navigate
-        """
-        
-        await send_payload_to_request_bin({"type": "location", "lat": lat, "lon": lon, "station_info": station_info})
+            final_message = f"""
+                You are currently in jurisdiction of:\n
+                {juridiction_station.get("name", "unknown")}\n
+                Address: {juridiction_station.get("address", "N/A")}\n\n
+                Google Maps Link: https://www.google.com/maps/dir/?api=1&destination={quote_plus(juridiction_station.get("address", 'N/A'))}&travelmode=driving&dir_action=navigate
+                \n\n           
+                
+                Nearest police station: {nearest_station.get("name", "unknown")} ({(nearest_station.get("distance_meters", 0)/1000).__format__(".2f")} km away)\n
+                Google Maps Link: https://www.google.com/maps/dir/?api=1&destination={quote_plus(nearest_station.get("address", 'N/A'))}&travelmode=driving&dir_action=navigate
+            """
+        except Exception as e:
+            await send_payload_to_request_bin({"type": "location", "lat": lat, "lon": lon, "station_info": station_info, "error": str(e)})
         
         await send_message_to_user(message=final_message, phone=phone)
         
